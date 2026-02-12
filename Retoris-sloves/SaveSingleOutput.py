2e0e5dabda7b2ce3d1f99bee623181b81e4009f0
Retoris <grassenjoyer78@gmail.com>
2026-02-12 18:26:47 +0100

    SSO upgrade

--- Retoris-sloves/SaveSingleOutput.py
+++ Retoris-sloves/SaveSingleOutput.py
@@ -1,5 +1,4 @@
 def SaveSingleOutput(filename,output):
     with open(filename,'wt',newline='\n',encoding='utf-8') as outfile:
-        if type(output) is int:
-            output = str(output)
+        output = str(output)
         outfile.write(output)
